6be5d6814cc9b96afda595d8fd7a53e644334dfc
Dmitry Koverznev <d.koverz@bk.ru>
2023-12-16 21:14:00 +0300

    9. Площадь треугольника. [#175474]

---
+++ src/main/java/ru/job4j/condition/TriangleArea.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class TriangleArea {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public static void main(String[] args) {
+        double a = 2;
+        double b = 2;
+        double c = 2;
+        double result = area(a, b, c);
+        System.out.printf("Square of triangle with sides " + a + ", " + b + ", " + c + " is %.2f%n", result);
+    }
+}
