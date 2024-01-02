6bd044e5bf833e13c64206c46cbe2ef1fbc51d8e
Dmitry Koverznev <d.koverz@bk.ru>
2024-01-02 15:39:38 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Length of array \"ages\" is " + ages.length);
+        System.out.println("Length of array \"surnames\" is " + surnames.length);
+        System.out.println("Length of array \"prices\" is " + prices.length);
     }
 }
