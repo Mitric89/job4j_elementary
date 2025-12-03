e18e26c5d38246bc745503a3b3027eabe173e39e
mitredo <dimon4ikac89@gmail.com>
2025-12-03 21:47:52 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,15 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println("Length of array \"ages\" is " + ages.length);
-        System.out.println("Length of array \"surnames\" is " + surnames.length);
-        System.out.println("Length of array \"prices\" is " + prices.length);
-
         String[] names = new String[4];
         names[0] = "Dmitry";
         names[1] = "Ivan";
         names[2] = "Alex";
         names[3] = "Nikita";
+        System.out.println("Length of array \"ages\" is " + ages.length);
+        System.out.println("Length of array \"surnames\" is " + surnames.length);
+        System.out.println("Length of array \"prices\" is " + prices.length);
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
