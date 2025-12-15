ce420b5a6ff86cbfaffd304e369750b9b973eddd
mitredo <dimon4ikac89@gmail.com>
2025-12-15 22:05:53 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,45 +8,33 @@ class PointTest {
 
     @Test
     void whenX10Y10X22Y20Then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double expected = 2;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(10, 10);
+        Point b = new Point(22, 20);
+        double result = a.distance(b);
+        assertThat(result).isEqualTo(15.62, withPrecision(0.01));
     }
 
     @Test
     void whenX1Minus1Y10X23Y2Minus3Then5() {
-        int x1 = -1;
-        int y1 = 0;
-        int x2 = 3;
-        int y2 = -3;
-        double expected = 5;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(-1, 10);
+        Point b = new Point(3, -3);
+        double result = a.distance(b);
+        assertThat(result).isEqualTo(13.6, withPrecision(0.01));
     }
 
     @Test
     void whenX1Minus1Y1Minus1X23Y2Minus3Then4Dot47() {
-        int x1 = -1;
-        int y1 = -1;
-        int x2 = 3;
-        int y2 = -3;
-        double expected = 4.47;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(-1, -1);
+        Point b = new Point(3, -3);
+        double result = a.distance(b);
+        assertThat(result).isEqualTo(4.47, withPrecision(0.01));
     }
 
     @Test
     void whenX1Minus5Y11X28Y20Then13Dot03() {
-        int x1 = -5;
-        int y1 = 1;
-        int x2 = 8;
-        int y2 = 0;
-        double expected = 13.03;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(-5, 1);
+        Point b = new Point(8, -0);
+        double result = a.distance(b);
+        assertThat(result).isEqualTo(13.04, withPrecision(0.01));
     }
 }
